9cf76b70b5ceb15a6bbac87dc97e1d45603d2c12
Ivan Betancourt <ivanab252@gmail.com>
2025-03-07 00:22:44 +0100

    Add and position school and experience logos

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 
 import ProfileBanner from '@/components/profilebanner';
+import Image from 'next/image';
 
 export default function Home() {
+
+    //todo: small screen bug, picture shows
+
     return (
         <>
             <div className='flex h-auto'>
@@ -14,8 +18,26 @@ export default function Home() {
                         </p>
                     </div>
                 </div>
-                <div className='lg:flex-1 lg:block sm:hidden'>
-                    Experiences
+                <div className='lg:flex lg:flex-col lg:flex-1 hidden items-center gap-12'>
+                    <section className='flex flex-col items-center'>
+                        <p className='text-2xl h-auto font-bold'>
+                            Education
+                        </p>
+                        <div className='flex justify-evenly py-5 h-auto'>
+                            <Image src='/amherst-college-two-line-seal.png' alt='Amherst College logo' width={300} height={100} />
+                            <Image src='/atc-logo.png' alt='The Academy of Technology and the Classics logo' width={100} height={100} />
+                        </div>
+                    </section>
+                    <section className='flex flex-col w-full items-center'>
+                        <p className='text-2xl h-auto font-bold'>
+                            Experience
+                        </p>
+                        <div className='flex flex-col flex-1 pt-5 gap-7 items-center'>
+                            <Image src='/lanl-logo.png' alt='Los Alamos National Laboratory logo' width={400} height={250} />
+                            <Image src='/the-amherst-student-logo.png' alt='The Amherst Student logo' width={400} height={250} />
+                            <Image src='/santa-fe-new-mexican-logo.png' alt='The Santa Fe New Mexican logo' width={420} height={250} />
+                        </div>
+                    </section>
                 </div>
             </div>
         </>
