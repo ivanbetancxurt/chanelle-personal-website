9400e1905cc29ac1624249046fdd240e864cbd72
Ivan Betancourt <ivanab252@gmail.com>
2025-03-14 01:46:37 +0100

    Make articles into links

--- components/Article.tsx
+++ components/Article.tsx
@@ -13,21 +13,23 @@ export default function Article({ article }: ArticleProps) {
     );
 
     return (
-        <div className='flex w-full gap-2 hover:bg-gray-200 p-3 rounded-2xl'>
-            <div className='relative h-[80px] w-[80px] rounded-xl overflow-hidden aspect-square'>
-                <Image src={article.thumbnail} alt={article.thumbnailDescription} fill className='object-cover'/>
-            </div>
-            
-            <div className='flex flex-col'>
-                <div className='flex gap-1 items-center'>
-                    {isNew ? (
-                        <div className='bg-green-400 h-3 w-3 rounded-full'></div>
-                    ) : null}
-                    <p className='font-bold'>{article.title}</p>
+        <a href={article.link} target='_blank' rel='noopener noreferrer'>
+            <div className='flex w-full gap-2 hover:bg-gray-200 p-3 rounded-2xl'>
+                <div className='relative h-[80px] w-[80px] rounded-xl overflow-hidden aspect-square'>
+                    <Image src={article.thumbnail} alt={article.thumbnailDescription} fill className='object-cover'/>
                 </div>
-                <p>{article.publication}</p>
-                <p className='right-auto'>{article.date}</p>
+            
+                <div className='flex flex-col'>
+                    <div className='flex gap-1 items-center'>
+                        {isNew ? (
+                            <div className='bg-green-400 h-3 w-3 rounded-full'></div>
+                        ) : null}
+                        <p className='font-bold'>{article.title}</p>
+                    </div>
+                    <p>{article.publication}</p>
+                    <p className='right-auto'>{article.date}</p>
+                </div>
             </div>
-        </div>
+        </a>
     );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function NavBar() {
-    const path = usePathname();
+    const path = usePathname(); // get current route to determine navbar state
 
     return (
         <Menubar className=''>
