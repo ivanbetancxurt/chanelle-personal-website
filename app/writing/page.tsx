cb59124b63cc1ce4a24b71592534b8795d1d85b3
Ivan Betancourt <ivanab252@gmail.com>
2025-05-22 01:25:37 -0400

    Make article filter form its own component

--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -2,24 +2,17 @@
 
 import ArticleList from '@/components/ArticleList';
 import articles from '@/articles.json';
-import { Formik, Form, Field } from 'formik';
 import { useState } from 'react';
 import AddArticleButton from '@/components/AddArticleButton';
 import { useViewModeContext } from '@/contexts/ViewModeContext';
-
-interface filterValues {
-    search: string,
-    organization: string,
-}
+import ArticleFilterForm from '@/components/ArticleFilterForm';
 
 // todo: make responsive
 
 export default function Writing() {
-    const initalValues: filterValues = { search: '', organization: 'All' } // initial values of filter form
-    const [search, setSearch] = useState(''); // article search state
-    const [organization, setOrganization] = useState('All'); // organization choice state
-    const [appliedMessageHidden, setAppliedMessageHidden] = useState(true); // filter confirmation showing flag
-    const { publicMode, toggleMode } = useViewModeContext(); // get mode context and toggler for the AddArticle button
+    const [search, setSearch] = useState<string>(''); // article search state
+    const [organization, setOrganization] = useState<string>('All'); // organization choice state
+    const { publicMode } = useViewModeContext(); // get mode context for the AddArticle button
 
     return (
         <>
@@ -28,50 +21,7 @@ export default function Writing() {
                     <ArticleList articles={articles} search={search} organization={organization} />   
                 </div>     
                 
-                <div className='absolute left-0 top-8 text-2xl'>
-                    <Formik
-                        initialValues={initalValues}
-                        onSubmit={(values) => {
-                            setSearch(values.search);
-                            setOrganization(values.organization);
-
-                            // display 'Filter applied!' message for 3 seconds after apply button pressed
-                            setAppliedMessageHidden(false);
-                            setTimeout(() => {
-                                setAppliedMessageHidden(true);
-                            }, 3000);
-                        }}
-                    >
-                        {() => {
-                            return (
-                                <Form>
-                                    <div className='flex flex-col gap-3'>
-                                        <Field id='search' name='search' placeholder='Search articles' className='underline focus:outline-none w-full' />
-                                        <div className='flex flex-col'>
-                                            <label htmlFor='organization'>Organization:</label>
-                                            <Field id='organization' name='organization' as='select' className='focus:outline-none'>
-                                                <option>All</option>
-                                                <option>The Amherst Student</option>
-                                                <option>Santa Fe New Mexican</option>
-                                                <option>LANL</option>
-                                            </Field>
-                                        </div>
-                                        
-                                        <div className='flex-col space-y-2'>
-                                            <button type='submit' className='bg-amber-200 w-fit hover:cursor-pointer hover:bg-amber-300 py-1 px-2 rounded-lg text-xl'>
-                                                Apply
-                                            </button>
-                                            <p className='text-sm text-green-400' hidden={appliedMessageHidden}>
-                                                Filter applied!
-                                            </p>
-                                        </div>
-                                        
-                                    </div>
-                                </Form>
-                            );
-                        }}
-                    </Formik>
-                </div>
+                <ArticleFilterForm setSearch={setSearch} setOrganization={setOrganization} />
 
                 {!publicMode && (
                     <AddArticleButton />
---
+++ components/ArticleFilterForm.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+
+interface ArticleFilterFormProps {
+    setSearch: (search: string) => void,
+    setOrganization: (organization: string) => void
+}
+
+interface filterValues {
+    search: string,
+    organization: string,
+}
+
+export default function ArticleFilterForm({ setSearch, setOrganization } : ArticleFilterFormProps) {
+    const initalValues: filterValues = { search: '', organization: 'All' } // initial values of filter form
+    const [appliedMessageHidden, setAppliedMessageHidden] = useState<boolean>(true); // filter confirmation showing flag
+
+    return (
+        <div className='absolute left-0 top-8 text-2xl'>
+            <Formik
+                initialValues={initalValues}
+                onSubmit={(values) => {
+                    setSearch(values.search);
+                    setOrganization(values.organization);
+
+                    // display 'Filter applied!' message for 3 seconds after apply button pressed
+                    setAppliedMessageHidden(false);
+                    setTimeout(() => {
+                        setAppliedMessageHidden(true);
+                    }, 3000);
+                }}
+            >
+                {() => {
+                    return (
+                        <Form>
+                            <div className='flex flex-col gap-3'>
+                                <Field id='search' name='search' placeholder='Search articles' className='underline focus:outline-none w-full' />
+                                <div className='flex flex-col'>
+                                    <label htmlFor='organization'>Organization:</label>
+                                    <Field id='organization' name='organization' as='select' className='focus:outline-none cursor-pointer'>
+                                        <option>All</option>
+                                        <option>The Amherst Student</option>
+                                        <option>Santa Fe New Mexican</option>
+                                        <option>LANL</option>
+                                    </Field>
+                                </div>
+                                
+                                <div className='flex-col space-y-2'>
+                                    <button type='submit' className='bg-amber-200 w-fit hover:cursor-pointer hover:bg-amber-300 py-1 px-2 rounded-lg text-xl'>
+                                        Apply
+                                    </button>
+                                    <p className='text-sm text-green-400' hidden={appliedMessageHidden}>
+                                        Filter applied!
+                                    </p>
+                                </div>
+                                
+                            </div>
+                        </Form>
+                    );
+                }}
+            </Formik>
+        </div>
+    );
+}
