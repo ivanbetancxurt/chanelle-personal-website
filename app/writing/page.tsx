0b7ff66cbb99e85a9b955c969a32613028da50fc
Ivan Betancourt <ivanab252@gmail.com>
2025-05-25 21:33:37 -0400

    Remove unnecessary wrapper

--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -1,5 +1,65 @@
-import WritingPage from '@/components/WritingPage';
+'use client'
 
-export default function Writing() {
-    return <WritingPage />;
+import ArticleList from '@/components/ArticleList';
+import { useState, useEffect } from 'react';
+import AddArticleButton from '@/components/AddArticleButton';
+import { useViewModeContext } from '@/contexts/ViewModeContext';
+import ArticleFilterForm from '@/components/FilterForm';
+import type { Articles } from '@/lib/generated/prisma';
+import AddArticleForm from '@/components/AddArticleForm';
+
+// todo: make responsive
+
+export default function WritingPage() {
+    const [articles, setArticles] = useState<Articles[]>([]); // state for articles
+    const [search, setSearch] = useState<string>(''); // article search state
+    const [organization, setOrganization] = useState<string>('All'); // organization choice state
+    const { publicMode } = useViewModeContext(); // get mode context for the AddArticle button
+    const [isChan, setIsChan] = useState<boolean>(false); // flag for whether this is chanelle
+    const [addArticlePressed, setAddArticlePressed] = useState<boolean>(false); // pressed state for AddArticle button
+
+    // fetch articles on component mount
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/articles`)
+            .then(res => {
+                if (!res.ok) throw new Error(`There was an error fetching articles: ${res.status}`);
+                return res.json();
+            })
+            .then(data => setArticles(data))
+            .catch(err => {console.error(err);});
+    }, []);
+
+    // set isChan flag depending on the state of the cookie via api
+    useEffect(() => { 
+        fetch('/api/amChan')
+            .then(res => {
+                if (!res.ok) throw new Error(`There was an error fetching who you are: ${res.status}`);
+                return res.json();
+            })
+            .then(({ isChan }) => setIsChan(isChan))
+            .catch(err => {console.error(err);});
+    }, []);
+
+    return (
+        <>
+            <div className='relative flex w-full flex-1 justify-center overflow-hidden h-full'>
+                <div className='absolute top-0 bottom-0 min-w-200'>
+                    <ArticleList articles={articles} search={search} organization={organization} />   
+                </div>     
+                
+                <ArticleFilterForm setSearch={setSearch} setOrganization={setOrganization} />
+
+                {isChan && !publicMode && (
+                    <AddArticleButton setPressed={setAddArticlePressed} />
+                )}
+
+                {addArticlePressed && !publicMode && (
+                    <AddArticleForm onArticleAdded={(article) => {
+                        setArticles([...articles, article]); // optimistically add the article to the list
+                        setAddArticlePressed(false); // close the form
+                    }} />
+                )}
+            </div>
+        </>
+    );
 }
