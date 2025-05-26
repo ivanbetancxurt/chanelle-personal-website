d1e66eb76a646c88b2b88967d0765999deed9497
Ivan Betancourt <ivanab252@gmail.com>
2025-05-26 00:14:29 -0400

    Fetch and download resume from resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { getURL } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
 export default function Resume() {
+    const [url, setUrl] = useState<string>(''); // state for resume url
+    const [loading, setLoading] = useState<boolean>(true); // loading state
+
+    // fetch and set url
+    useEffect(() => {
+        setUrl(getURL('thumbnails', 'BetancourtIvan.pdf'));
+        setLoading(false);
+    });
+
     return (
         <div className='flex w-full justify-center items-center'>
-            <a href="/atc-logo.png" download className='font-bold text-2xl'>
+            {loading ? (
+                <div className='animate-spin rounded-full h-13 w-13 border-9 border-gray-300 border-t-amber-200 mt-[200px]' />
+            ) : (
+                <iframe src={url} />
+            )}
+            
+            <a href={url} download target='_blank' rel='noopener noreferrer' className='font-bold text-2xl'>
                 Download
             </a>
         </div>
--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -54,7 +54,7 @@ export default function WritingPage() {
             <div className='relative flex w-full flex-1 justify-center overflow-hidden h-full'>
                 <div className='flex absolute top-0 bottom-0 min-w-200 justify-center'>
                     {loading ? (
-                        <div className='animate-spin rounded-full h-13 w-13 border-9 border-gray-300 border-t-amber-200 mt-[200px]'></div>
+                        <div className='animate-spin rounded-full h-13 w-13 border-9 border-gray-300 border-t-amber-200 mt-[200px]' />
                     ) : (
                         <ArticleList articles={articles} search={search} organization={organization} />
                     )}  
