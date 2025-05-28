f912f456bf55171d433613c73e75da0de66e28ff
Ivan Betancourt <ivanab252@gmail.com>
2025-05-28 12:58:14 -0400

    Add delete icon to articles

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,11 +1,12 @@
 import { Articles } from '@/lib/generated/prisma';
 import Image from 'next/image';
 import { isNew } from '@/lib/utils';
+import { FaTrashAlt } from "react-icons/fa";
 
 export default function Article({ article }: { article: Articles }) {
     return (
         <a href={article.link} target='_blank' rel='noopener noreferrer'>
-            <div className='flex w-full gap-2 hover:bg-gray-200 p-3 rounded-2xl'>
+            <div className='flex w-full gap-2 hover:bg-gray-200 p-3 rounded-2xl items-center'>
                 <div className='relative h-[80px] w-[80px] rounded-xl overflow-hidden aspect-square'>
                     <Image 
                         src={article.thumbnail} 
@@ -16,7 +17,7 @@ export default function Article({ article }: { article: Articles }) {
                     />
                 </div>
             
-                <div className='flex flex-col'>
+                <div className='flex flex-col w-[600px]'>
                     <div className='flex gap-2 items-center'>
                         {isNew(article) ? (
                             <div className='bg-green-400 h-3 w-3 rounded-full'></div>
@@ -26,6 +27,8 @@ export default function Article({ article }: { article: Articles }) {
                     <p>{article.organization}</p>
                     <p className='right-auto'>{article.date}</p>
                 </div>
+
+                <FaTrashAlt color='red' className='ml-2' />
             </div>
         </a>
     );
