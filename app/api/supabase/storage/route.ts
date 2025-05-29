a5fe98872ae1495184a8630884eb4f7156887e8c
Ivan Betancourt <ivanab252@gmail.com>
2025-05-29 19:17:20 -0400

    Use service role key for thumbnail upload instead

--- app/api/supabase/storage/route.ts
+++ app/api/supabase/storage/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!); // export supabase client
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!); // export supabase client
 
 // store the provided image into storage and return its name (URL)
 export async function POST(request: NextRequest) {
