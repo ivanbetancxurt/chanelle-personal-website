c2c34854ba1d74618654b6f72bcd38a05423a876
Ivan Betancourt <ivanab252@gmail.com>
2025-05-29 20:00:36 -0400

    Use singleton pattern for prisma

--- app/api/supabase/articles/route.ts
+++ app/api/supabase/articles/route.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/lib/prisma';
 import { createClient } from '@supabase/supabase-js';
 
-const prisma = new PrismaClient(); // initialize prisma client
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!); // initialize supabase client
 
 
--- app/api/supabase/bio/route.ts
+++ app/api/supabase/bio/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient(); // initialize prisma client
+import { prisma } from '@/lib/prisma';
 
 // get current bio
 export async function GET() {
